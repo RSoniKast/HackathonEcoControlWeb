b872a67d4981a2e56c0171a7a145f39311ec6ed1
QuentinRod <quentin.rodriguez@estiam.com>
2024-06-10 15:52:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Économie d'énergie</title>
+    <title>EcoControl</title>
     <link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
 <body>
     <header>
-        <h1>Économie d'énergie</h1>
+        <h1>EcoControl</h1>
     </header>
     <main>
         <section>
-            <h2>Votre économie d'énergie</h2>
+            <h2>Votre énergie dépenser</h2>
             <div id="kwh" class="kwh"></div>
         </section>
     </main>
