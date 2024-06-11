3a16d21f146f933c2896877ad87492c65d033fdd
QuentinRod <quentin.rodriguez@estiam.com>
2024-06-11 17:00:41 +0200

    Modification jour 2

--- index.php
+++ index.php
@@ -1,6 +1,38 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mesure";
+
+// Créer une connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT Humidité, Température, Tension, Courant FROM mesurestable LIMIT 1";
+$result = $conn->query($sql);
+
+$data = array(); // Ajouté cette ligne
+
+if ($result->num_rows > 0) {
+  // Récupérer les données de chaque ligne
+  while($row = $result->fetch_assoc()) {
+    $data = $row; // Modifié cette ligne
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EcoControl</title>
     <link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
@@ -10,10 +42,32 @@
     </header>
     <main>
         <section>
-            <h2>Votre énergie dépenser</h2>
-            <div id="kwh" class="kwh"></div>
+            <h2>Energie / Température / Humidité</h2>
+            <div class="circle-container">
+                <div id="kwh" class="circle">
+                    <svg width="120" height="120">
+                        <circle r="50" cx="60" cy="60" />
+                    </svg>
+                    <span class="circle-text"></span>
+                </div>
+                <div id="temperature" class="circle">
+                    <svg width="120" height="120">
+                        <circle r="50" cx="60" cy="60" />
+                    </svg>
+                    <span class="circle-text"></span>
+                </div>
+                <div id="humidity" class="circle">
+                    <svg width="120" height="120">
+                        <circle r="50" cx="60" cy="60" />
+                    </svg>
+                    <span class="circle-text"></span>
+                </div>
+            </div>
         </section>
     </main>
+    <script>
+    var data = <?php echo json_encode($data); ?>; // Ajouté cette ligne
+    </script>
     <script src="js/index.js"></script>
 </body>
 </html>
