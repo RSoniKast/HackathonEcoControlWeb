84b44079cf0de2bd6a93d12227322ff779cff683
SoniKast1 <balla.diakite57@outlook.fr>
2024-06-11 10:58:12 +0200

    implémentation du code de quentin + modification de la page d'accueil

--- index.php
+++ index.php
@@ -1,26 +1,82 @@
 <!doctype html>
-<html lang="en">
+<html lang="fr">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>EcoControl - Page d'accueil</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
-  <div class="container">
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <span class="fs-4">
-            <bold><span class="text-primary">Eco</span><span class="text-success">Control</bold></span>
-        </span>
-      </a>
+  <nav class="navbar fixed-top navbar-light bg-light">
+    <div class="container">
+        <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+          <span class="fs-4">
+              <bold><span class="text-primary">Eco</span><span class="text-success">Control</bold></span>
+          </span>
+        </a>
+        <ul class="nav nav-pills">
+          <li class="nav-item"><a href="#" class="nav-link link-secondary" aria-current="page">Accueil</a></li>
+          <li class="nav-item"><a href="#" class="nav-link text-success">À Propos</a></li>
+          <li class="nav-item"><a href="#" class="nav-link text-success">FAQs</a></li>
+          <li><a class="btn btn-outline-success me-2" href="user/dashboard" role="button">Connexion</a></li>
+          <li><button type="button" class="btn btn-success">S'inscrire</button></li>
+        </ul>
+    </div>
+  </nav>
 
-      <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link link-secondary" aria-current="page">Accueil</a></li>
-        <li class="nav-item"><a href="#" class="nav-link text-success">À Propos</a></li>
-        <li class="nav-item"><a href="#" class="nav-link text-success">FAQs</a></li>
-        <li><a class="btn btn-outline-success me-2" href="user/dashboard" role="button">Connexion</a></li>
-        <li><button type="button" class="btn btn-success">S'inscrire</button></li>
-      </ul>
-    </header>
+  <main>
+  <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-secondary" style="background-image: url('img/ecocontrolbg.jpg');">
+    <div class="col-md-6 p-lg-5 mx-auto my-5">
+      <h1 class="display-1 text-white">Jouez, économisez, gagnez</h1>
+      <h4 class="fw-normal text-white">Avec EcoControl, l'économie d'énergie n'a jamais été aussi ludique</h4>
+      <div class="d-flex gap-3 justify-content-center lead fw-normal">
+        <button type="button" class="btn btn-success">Inscrivez-vous</button>
+      </div>
+    </div>
+    <div class="product-device shadow-sm d-none d-md-block"></div>
+    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
   </div>
+
+  <footer class="container py-5">
+  <div class="row">
+    <div class="col-12 col-md">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mb-2" role="img" viewBox="0 0 24 24"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
+      <small class="d-block mb-3 text-body-secondary">&copy; 2017–2024</small>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Features</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="link-secondary text-decoration-none" href="#">Cool stuff</a></li>
+        <li><a class="link-secondary text-decoration-none" href="#">Random feature</a></li>
+        <li><a class="link-secondary text-decoration-none" href="#">Team feature</a></li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Resources</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="link-secondary text-decoration-none" href="#">Resource name</a></li>
+        <li><a class="link-secondary text-decoration-none" href="#">Resource</a></li>
+        <li><a class="link-secondary text-decoration-none" href="#">Another resource</a></li>
+        <li><a class="link-secondary text-decoration-none" href="#">Final resource</a></li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Resources</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="link-secondary text-decoration-none" href="#">Business</a></li>
+        <li><a class="link-secondary text-decoration-none" href="#">Education</a></li>
+        <li><a class="link-secondary text-decoration-none" href="#">Government</a></li>
+        <li><a class="link-secondary text-decoration-none" href="#">Gaming</a></li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>About</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="link-secondary text-decoration-none" href="#">Team</a></li>
+        <li><a class="link-secondary text-decoration-none" href="#">Locations</a></li>
+        <li><a class="link-secondary text-decoration-none" href="#">Privacy</a></li>
+        <li><a class="link-secondary text-decoration-none" href="#">Terms</a></li>
+      </ul>
+    </div>
+  </div>
+</footer>
 </html>
