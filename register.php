49fe5ce401c44631d34eab5095cb104267ee6873
SoniKast1 <balla.diakite57@outlook.fr>
2024-06-12 10:28:48 +0200

    Update register.php

--- register.php
+++ register.php
@@ -29,7 +29,7 @@ if (isset($_GET['logout'])) {
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
-        <title>TrackBase - Créer un compte</title>
+        <title>EcoControl - Créer un compte</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
     <body class="flex align-items-center py-4 bg-body-tertiary">
